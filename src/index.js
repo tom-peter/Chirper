69ce84b4d02661349974909f992263326fa82d5a
Tamas Peter <me@example.com>
2020-05-22 12:56:13 +0200

    feat: Create store and apply middleware
     - install redux, react-redux, redux-thunk
     - create logger middleware
     - create store
     - apply thunk and logger middleware
     - add Redux Provider code to entry point (App)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+// import the default exports from the index.js files from the given folders
+import reducer from './reducers';
+import middleware from './middleware';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// Create the store and
+// pass the middleware as the second argument
+const store = createStore(reducer, middleware);
+
+// Wrap the App component into react-redux's context Provider
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
